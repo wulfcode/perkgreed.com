7b52cef2bf4e80b885ac2d7934ed7021bd0e616b
Sean Prasertsit <sean@wulfcode.com>
2017-09-09 20:36:36 -0700

    :heavy_plus_sign: stylus and loader

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app/App.jsx';
+import Σapp from './app/app.styl';
 
 ReactDOM.render(<App />, document.getElementById('root'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,15 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const sGrid = require('s-grid');
 
 const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
   template: './src/index.html',
   filename: 'index.html',
   inject: 'body',
 });
+
+const ExtractTextPluginConfig = new ExtractTextPlugin('styles.css');
 
 module.exports = {
   entry: './src/app.js',
@@ -20,9 +24,26 @@ module.exports = {
         use: 'babel-loader',
         exclude: /node_modules/,
       },
+      {
+        test: /\.styl$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [
+            'css-loader',
+            {
+              loader: 'stylus-loader',
+              options: {
+                use: [sGrid()],
+              },
+            },
+          ],
+        }),
+        exclude: /node_modules/,
+      },
     ],
   },
   plugins: [
     HtmlWebpackPluginConfig,
+    ExtractTextPluginConfig,
   ],
 };
