92efa8b64e76efb183c1eaed3c75d8300216f09c
Sean Prasertsit <sean@wulfcode.com>
2017-11-24 22:58:00 -0800

    :rotating_light: remove lint warning for style import

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app/App.jsx';
+
+//eslint-disable-next-line
 import Σapp from './app/app.styl';
 
 ReactDOM.render(<App />, document.getElementById('root'));
